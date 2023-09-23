c1e773ec2c16ae9f80869a3bc599da3558a09ccd
Erwin Lejeune <erwin.lejeune15@gmail.com>
2023-09-24 01:32:38 +0400

    Fix weird layout

--- src/services/explorers/taiko.ts
+++ src/services/explorers/taiko.ts
@@ -1,9 +1,22 @@
 import StandardExplorerService from './standard_explorer.ts';
 import { ETH_TOKEN } from '../../common/common.ts';
+import { Transaction } from './explorer.ts';
 
 class TaikoExplorerService extends StandardExplorerService {
   constructor() {
-    super('explorer.test.taiko.xyz', "taiko", "./chains/taiko.svg", 'https://explorer.test.taiko.xyz', ETH_TOKEN);
+    super('blockscoutapi.jolnir.taiko.xyz', 'taiko', './chains/taiko.svg', 'https://blockscoutapi.jolnir.taiko.xyz/', ETH_TOKEN);
+  }
+
+  needInternalTx(): boolean {
+    return true;
+  }
+
+  isFromBridge(tx: Transaction): boolean {
+    if (!tx.from) return false;
+    if (tx.from.toLowerCase() === '0x1000777700000000000000000000000000000004'.toLowerCase()) {
+      return true;
+    }
+    return false;
   }
 }
 
