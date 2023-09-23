c6e7260669a60f6b1701657412c5bee867f638fc
Erwin Lejeune <erwin.lejeune15@gmail.com>
2023-09-23 20:10:06 +0400

    Fix Warnings

--- src/services/explorers/nova.ts
+++ src/services/explorers/nova.ts
@@ -1,14 +1,9 @@
 import StandardExplorerService from './standard_explorer.ts';
 import { ETH_TOKEN } from '../../common/common.ts';
-import { Transaction } from './explorer.ts';
 
 class NovaExplorerService extends StandardExplorerService {
   constructor() {
     super('nova-explorer.arbitrum.io', "nova", "./chains/nova.svg", 'https://nova-explorer.arbitrum.io/', ETH_TOKEN);
-  }
-
-  isFromBridge(tx: Transaction): boolean {
-    return false;
   }
 }
 
